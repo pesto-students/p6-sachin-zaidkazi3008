ff1cf73b80b316c605a7f25d7388f2bad4ae9f1d
zaidkazi3008 <zaidkazi.in@gmail.com>
2023-02-11 10:57:31 +0530

    sentry added on react app

---
+++ Week-16/urlshortner/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './App.css';
+import Footer from './components/footer/Footer';
+import LinkInput from './components/linkInput/LinkInput';
+import LinkResult from './components/linkResult/LinkResult';
+import Navbar from './components/navbar/Navbar';
+import * as Sentry from "@sentry/react";
+
+function App() {
+  const [inputValue, setInputValue] = useState("")
+
+  return (
+    <div className="app">
+      <Navbar/>
+      <main className='main'>
+      <LinkInput setInputValue={setInputValue}/>
+      <LinkResult inputValue={inputValue} />
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+// export default App;
+export default Sentry.withProfiler(App);
